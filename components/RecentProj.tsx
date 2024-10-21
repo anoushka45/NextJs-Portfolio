6692ff0dc4e2f20752e2fb8c05ec0454ca405fb1
anoushka <vyasanoushka@gmail.com>
2024-10-21 23:21:14 +0530

    Update main page content

---
+++ components/RecentProj.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { projects } from '@/data'
+import { PinContainer } from './ui/3d-pin'
+import { FaLocationArrow } from 'react-icons/fa'
+import { techStackIcons } from '@/data'
+const RecentProj = () => {
+  return (
+    <div className='py-20' id="projects">
+      <h1 className='heading'>A small selection of {' '} <span className='text-purple'>recent projects</span></h1>
+
+      <div className="flex justify-between items-center gap-6 mt-12">
+  {techStackIcons.map((icon, index) => (
+    <div key={index} className="w-10 h-10 flex justify-center items-center transition-transform duration-300 hover:rotate-12">
+      <img src={icon} alt="Tech Stack Icon" className="w-full h-full" />
+    </div>
+  ))}
+</div>
+
+      <div className='flex flex-wrap items-center justify-center p-4 
+      gap-x-24 gap-y-8 mt-10'>
+        {
+          projects.map(({
+            id, title, des, img, iconLists, link
+          }) => (
+            <div className='lg:min-h-[32.5rem]
+          flex items-center justify-center
+           w-[80vw] sm:h-[41rem] h-[32rem] sm:w-[570px]
+          '
+              key={id}>
+             <PinContainer title={link} >
+              <div className='relative flex items-center justify-center
+              sm:w-[570px] w-[80vw] overflow-hidden h-[30vh] mb-10  sm:h-[40vh]'>
+                <div className='relative w-full h-full overflow-hidden lg:rounded-3xl
+                bg-[#13162d]'>
+                  <img src="/bg.png" alt="bg-img" />
+                </div>
+                <img src={img} alt={title} 
+                className='z-10 absolute bottom-0'
+                />
+              </div>
+              <h1 className='font-bold lg:text-2xl md:text-xl
+              text-base line-clamp-1'>{title}</h1>
+              <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>{des}</p>
+              <div className='flex items-center justify-between mt-7 mb-3'>
+                <div className='flex items-center'>
+                  {iconLists.map((icon,index)=>(
+                    <div key={icon} className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8
+                    flex items-center justify-center' style={{
+                      transform: `translateX(-${5*index*2}px)`
+                    }}>
+                      <img src={icon} alt={icon} className='p-2' />
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className='flex justify-center items-center mb-2'>
+                <a href={link} className='flex lg:text-xl md:text-xs text-sm text-purple'>view details</a>
+              </div>
+             </PinContainer>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default RecentProj
---
+++ components/hero.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { FaLocationArrow } from 'react-icons/fa';
+import { TextGenerateEffect } from './ui/text-generate-effect';
+import { Spotlight } from './ui/spotlight';
+import MagicButton from './ui/MagicButton';
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+      <div>
+        <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 hscreen' fill='white' />
+        <Spotlight className='top-10 left-full h-[80vh]  w-[50vw]' fill='purple' />
+        <Spotlight className='top-28 left-80 h-[80vh]  w-[50vw]' fill='blue' />
+      </div>
+
+
+      <div className="h-screen w-full dark:bg-black-100 bg-white  
+      dark:bg-grid-white/[0.03] bg-grid-black/[0.2] 
+      flex items-center justify-center absolute top-0 left-0">
+        <div className="absolute pointer-events-none 
+        inset-0 flex items-center justify-center dark:bg-black-100 bg-white
+        [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
+      </div>
+
+
+
+
+
+    
+
+      <div className="flex justify-center relative z-10 my-10">
+  <div className="text-center text-[40px] md:text-4xl lg:text-6xl flex flex-col items-center justify-center">
+  <p className="bg-clip-text text-transparent bg-gradient-to-r from-violet-600 to-pink-300 font-bold hover:from-pink-400 hover:to-violet-600 hover:shadow-lg transition duration-300">
+  ANOUSHKA VYAS
+</p>
+
+  </div>
+</div>
+
+<TextGenerateEffect 
+  words='Shaping Concepts into Immersive Digital Experiences' 
+  className='text-center text-[40px] md:text-4xl lg:text-5xl' 
+/>
+
+
+      <p className='bg-clip-text text-transparent bg-gradient-to-r from-pink-200 to-blue-400  hover:from-pink-300 hover:to-blue-500 text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl relative'>
+
+
+       A Web Developer and final-year IT student passionate about building intuitive, user-friendly web applications. I love transforming ideas into fully functional digital experiences. Take a look at my work and see what I’ve been creating!
+
+</p>
+  
+
+<a href="/docs/resume.pdf" target="_blank" rel="noopener noreferrer">
+  <MagicButton title="Show my work" 
+    icon={<FaLocationArrow />}
+    position='right' />
+</a>
+
+    </div>
+
+  )
+}
+
+export default Hero;
